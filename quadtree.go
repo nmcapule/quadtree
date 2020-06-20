6067ec53fa487c84cdfd41cfb94520e9a7859323
Nathaniel Capule <nathaniel.capule@celconsulting.com>
2020-06-20 16:25:01 +0000

    The quadtree can now move objects. Homygash.

--- quadtree.go
+++ quadtree.go
@@ -9,6 +9,7 @@ var (
 	errAlreadySplit      = errors.New("quadtree is already split")
 	errInsertOutOfBounds = errors.New("object to be inserted is out-of-bounds")
 	errInsertFailure     = errors.New("object insert failure")
+	errObjectNotFound    = errors.New("object is not in the quadtree")
 )
 
 // Object encapsulates any object inserted into the quadtree.
@@ -43,6 +44,7 @@ type Quadtree struct {
 	Objects    []*Object
 	Level      int
 	MaxObjects int
+	Total      int
 }
 
 // NewQuadtree instantiates a new quadtree from a given bounds.
@@ -63,8 +65,11 @@ func (qt *Quadtree) Insert(object *Object) error {
 	if len(qt.Nodes) == 0 {
 		if len(qt.Objects) < qt.MaxObjects {
 			qt.Objects = append(qt.Objects, object)
+			qt.Total++
 			return nil
 		}
+		// Remove previous total to reset counter.
+		qt.Total -= qt.MaxObjects
 		// If can't insert, subdivide.
 		err := qt.subdivide()
 		if err != nil {
@@ -75,6 +80,7 @@ func (qt *Quadtree) Insert(object *Object) error {
 		err := node.Insert(object)
 		// If insert is successful, terminate and return nil error.
 		if err == nil {
+			qt.Total++
 			return nil
 		}
 		if err == errInsertOutOfBounds {
@@ -83,6 +89,57 @@ func (qt *Quadtree) Insert(object *Object) error {
 		return err
 	}
 	return errInsertFailure
+}
+
+// Removes an object in the quadtree.
+func (qt *Quadtree) Remove(object *Object) error {
+	// If leaf node, do a simple iterate.
+	if qt.Nodes == nil {
+		// We could optimize this by making a set instead of array maybe?
+		idx := -1
+		for i, curr := range qt.Objects {
+			if *curr == *object {
+				idx = i
+			}
+		}
+		if idx != -1 {
+			qt.Total--
+			swap := len(qt.Objects) - 1
+			qt.Objects[idx], qt.Objects[swap] = qt.Objects[swap], qt.Objects[idx]
+			qt.Objects = qt.Objects[:swap]
+			return nil
+		}
+	}
+
+	// Declare cleanup ops. If there are no more nodes after the removal, dealloc nodes.
+	defer func(qt *Quadtree) {
+		if qt.Total == 0 {
+			qt.Nodes = nil
+		}
+	}(qt)
+
+	// Recursively remove the object from the children nodes.
+	for _, node := range qt.Nodes {
+		if !node.Bounds.Within(object.X, object.Y) {
+			continue
+		}
+		// Update total and remove object.
+		qt.Total--
+		return node.Remove(object)
+	}
+
+	return errObjectNotFound
+}
+
+// Moves an object to another location in the quadtree.
+func (qt *Quadtree) Move(object *Object, x, y float64) error {
+	err := qt.Remove(object)
+	if err != nil {
+		return err
+	}
+	object.X = x
+	object.Y = y
+	return qt.Insert(object)
 }
 
 // FindAllWithin retrieves all objects that intersects the given bound within the quadtree.
--- quadtree_test.go
+++ quadtree_test.go
@@ -7,14 +7,14 @@ import (
 	"testing"
 )
 
-func createQuadtree(n int) *Quadtree {
-	qt := NewQuadtree(Bounds{0., 0., 100., 100.})
+func createQuadtree(n int, bound Bounds) *Quadtree {
+	qt := NewQuadtree(bound)
 
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < n; i++ {
 		err := qt.Insert(&Object{
-			X:    rand.Float64() * 100,
-			Y:    rand.Float64() * 100,
+			X:    bound.X + rand.Float64()*bound.Width,
+			Y:    bound.Y + rand.Float64()*bound.Height,
 			Data: i,
 		})
 		if err != nil {
@@ -25,39 +25,80 @@ func createQuadtree(n int) *Quadtree {
 	return qt
 }
 
-func benchmarkCreate(n int, b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		createQuadtree(n)
-	}
-}
-
 func benchmarkFindAllWithin(qt *Quadtree, b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		// x, y := 0., 0.
 		// w, h := 100., 100.
-		x := rand.Float64() * 90
-		y := rand.Float64() * 90
-		w := 10. //rand.Float64() * 5
-		h := 10. //rand.Float64() * 5
+		x := rand.Float64() * 99
+		y := rand.Float64() * 99
+		w := 1. //rand.Float64() * 5
+		h := 1. //rand.Float64() * 5
 		qt.FindAllWithin(Bounds{x, y, w, h})
 	}
 }
 
 func BenchmarkQuadtreeFind(b *testing.B) {
-	qt := createQuadtree(10000)
+	qt := createQuadtree(10000, Bounds{0, 0, 100, 100})
 	b.ResetTimer()
 	benchmarkFindAllWithin(qt, b)
 }
 
-func BenchmarkQuadtreeCreate1000(b *testing.B) {
-	benchmarkCreate(1000, b)
+func benchmarkCreate(n int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		createQuadtree(n, Bounds{0, 0, 100, 100})
+	}
 }
-func BenchmarkQuadtreeCreate10000(b *testing.B) {
-	benchmarkCreate(10000, b)
+
+func BenchmarkQuadtreeCreate100(b *testing.B) {
+	benchmarkCreate(100, b)
 }
-func BenchmarkQuadtreeCreate100000(b *testing.B) {
-	benchmarkCreate(100000, b)
+
+func benchmarkMove(qt *Quadtree, objects []*Object, bounds Bounds, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		x := bounds.X + rand.Float64()*bounds.Width
+		y := bounds.Y + rand.Float64()*bounds.Height
+		qt.Move(objects[n%qt.Total], x, y)
+	}
 }
-func BenchmarkQuadtreeCreate1000000(b *testing.B) {
-	benchmarkCreate(1000000, b)
+
+func BenchmarkQuadtreeMove(b *testing.B) {
+	bounds := Bounds{0, 0, 100, 100}
+	qt := createQuadtree(1000000, bounds)
+	objects := qt.FindAllWithin(bounds)
+	b.ResetTimer()
+	benchmarkMove(qt, objects, bounds, b)
+}
+
+func TestInsertRemove(t *testing.T) {
+	bounds := Bounds{0, 0, 100, 100}
+	n := 5000
+	qt := createQuadtree(n, bounds)
+	if qt.Total != n {
+		t.Errorf("Wrong number of total objects: want %d, got %d", n, qt.Total)
+	}
+	objects := qt.FindAllWithin(bounds)
+	if len(objects) != n {
+		t.Errorf("Wrong number of retrieved objects: want %d, got %d", n, len(objects))
+	}
+	for _, obj := range objects {
+		x := bounds.X + rand.Float64()*bounds.Width
+		y := bounds.Y + rand.Float64()*bounds.Height
+		qt.Move(obj, x, y)
+	}
+	objects = qt.FindAllWithin(bounds)
+	if len(objects) != n {
+		t.Errorf("Wrong number of retrieved objects after move: want %d, got %d", n, len(objects))
+	}
+	for i, obj := range objects {
+		err := qt.Remove(obj)
+		if err != nil {
+			t.Errorf("Unexpected error on object Remove: %v", err)
+		}
+		if qt.Total != n-i-1 {
+			t.Errorf("Wrong number of total objects after removal: want %d, got %d", n-i-1, qt.Total)
+		}
+	}
+	if qt.Nodes != nil {
+		t.Errorf("Expecting child nodes to be nil, got %+v", qt.Nodes)
+	}
 }
